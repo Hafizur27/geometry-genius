8d5aab97132027b5dbb361da882c85604dec52ba
hafizur27 <mahmadhafiz33@gmail.com>
2023-02-18 00:19:49 +0600

    navigation part

---
+++ js/question.js
@@ -0,0 +1,5 @@
+// question html file add in blog  button when click blog button
+
+document.getElementById('blog-btn').addEventListener('click', function(){
+    window.location.href = 'question.html';
+})
